54a3f0486e2f92694be5a8ae9bb022f0820d3d22
hang1038 <31604301+hang1038@users.noreply.github.com>
2018-01-15 21:03:02 +0800

    ~

---
+++ Assets/Script/Customer/CustomerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomerController : MonoBehaviour
+{
+    private Inventory inventory;
+    public CustomerSpawn CS;
+
+    public void CheckFood()
+    {
+        inventory = FindObjectOfType<Inventory>();
+        inventory.GetItemInList();
+        Item[] PlayerSelectedFood = inventory.GetItemInList();
+		CS = FindObjectOfType<CustomerSpawn> ();
+		Recipe CorrectRecipeFoods = Resources.Load<Recipe>("recipes/"+CS.chosenDish);
+        int counter = CorrectRecipeFoods.foods.Length;
+        int PlayerSelectedLength = PlayerSelectedFood.Length;
+        for (var i = 0; i < CorrectRecipeFoods.foods.Length; i++)
+        {
+            bool correct = false;
+            for (var k = 0; k < PlayerSelectedLength; k++)
+            {
+                if (CorrectRecipeFoods.foods[i] == PlayerSelectedFood[k])
+                {
+                    PlayerSelectedFood[k] = null;
+                    counter--;
+                    Debug.Log("counter:"+counter);
+                    correct = true;
+                    break;
+                }
+            }
+
+           if (correct == false)
+            {
+                Debug.Log("Selected Food incorrect");
+                return;
+            }
+            if (counter == 0)
+            {
+
+            for (var k = 0; k < PlayerSelectedLength; k++)
+            {
+                if (PlayerSelectedFood[k] != null)
+                {
+                        Debug.Log("Selected Food incorrect");
+                        return;
+                    }
+            }
+                Debug.Log("Selected Food correct!!!!!");
+                CS = FindObjectOfType<CustomerSpawn>();
+                CS.destroyCustomer();
+            }
+        }
+    }
+}
---
+++ Assets/Script/Customer/CustomerSpawn.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomerSpawn : MonoBehaviour {
+
+    public List<GameObject> customers;
+    public GameObject currentCustomer;
+	public RecipeManager rm;
+	string[] dish;
+	public string chosenDish;
+	public Text text;
+
+    GameObject randCustomer;
+    public int totalCustomer;
+    public int customerServed = 0;
+
+    int spawnWait;
+    public int minSpawnWait;
+    public int maxSpawnWait;
+
+    public bool killMode;
+    
+	void Start ()
+    {
+        StartCoroutine(spawnCustomer());
+	}
+
+    void Update()
+    {
+        if (killMode)
+        {
+            killMode = false;
+
+            if (currentCustomer)
+            {
+                destroyCustomer();
+            }
+        }
+
+        // End game
+        if (customerServed == totalCustomer)
+        {
+
+        }
+    }
+
+    IEnumerator spawnCustomer()
+    {
+		yield return new WaitUntil(() => rm.isStart);
+		List<string> recipe = new List<string> (rm.chosenDish);
+		dish = new string[totalCustomer];
+
+
+        for (int i = 0; i < totalCustomer; i++)
+        {
+            yield return new WaitUntil(() => !currentCustomer);
+            spawnWait = Random.Range(minSpawnWait, maxSpawnWait);
+            yield return new WaitForSeconds(spawnWait);
+
+			dish[i] = recipe[Random.Range (0, recipe.Count)];
+			text.text = dish[i];
+			text.enabled = true;
+			chosenDish = dish[i];
+
+			if (totalCustomer - customerServed <= recipe.Count) {
+				recipe.Remove(dish[i]);
+			}
+
+            randCustomer = customers[Random.Range(0, customers.Count)];
+            currentCustomer = Instantiate(randCustomer, transform.position, transform.rotation);
+            customers.Remove(randCustomer);
+            currentCustomer.transform.parent = gameObject.transform;
+        }
+    }
+
+    public void destroyCustomer()
+    {
+        Destroy(currentCustomer);
+		text.enabled = false;
+		text.text = "";
+        currentCustomer = null;
+        customerServed++;
+    }
+}
